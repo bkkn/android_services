a501fefbab5431d0b47e6a6b51ae2708ea7036ab
bakaikin <bakaikin@yandex.ru>
2022-07-28 10:44:37 +0300

    Initial commit

---
+++ app/src/main/java/me/bkkn/myservicesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.bkkn.myservicesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
